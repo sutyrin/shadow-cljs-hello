af99f029c5f1e50fb260182a9f685e21e8d9175d
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2023-09-04 13:11:39 +0300

    init

---
+++ app/cljs-runtime/flashcards_demo.core.js
@@ -0,0 +1,7 @@
+goog.provide('flashcards_demo.core');
+cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["Hello world (module level)!"], 0));
+flashcards_demo.core.main = (function flashcards_demo$core$main(){
+return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["Hello world (main level)!"], 0));
+});
+
+//# sourceMappingURL=flashcards_demo.core.js.map
